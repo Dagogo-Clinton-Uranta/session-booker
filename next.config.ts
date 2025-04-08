bd1ee680fb3aa5381b4cebb2be59197cb4add0ce
Dagogo-Clinton-Uranta <dagogouranta@gmail.com>
2025-04-08 12:20:00 +0100

    ignored eslint in prod

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+  eslint: {
+    ignoreDuringBuilds: true, // ⚠️ Disables blocking deploys on lint errors
+  },
 };
 
 
